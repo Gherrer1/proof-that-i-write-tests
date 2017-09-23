0665f52cc52235627ac511bbc8a155e2f5181f7b
Gerardo Herrera <gherrer1@binghamton.edu>
2017-09-23 18:39:13 -0400

    login.ejs template done and basic GET and POST routes to /login, nowhere near done

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const logger		= require('morgan');
 const mongoose		= require('mongoose');
 const config		= require('./config'),
 	  DB_URL		= config.DB_URL;
+const app = express();
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect(DB_URL, { useMongoClient: true })
 .then(
-	function connectSuccess() { console.log(`Connected to ${DB_URL}`) } 
+	function connectSuccess() { console.log(`Connected to ${DB_URL}`) }
 		,
 	function connectFail(err) {
 		console.log('Connection Error:\n', err);
@@ -17,13 +18,26 @@ mongoose.connect(DB_URL, { useMongoClient: true })
 	}
 );
 
-const app = express();
+// config
+app.set('view engine', 'ejs');
+
+// Middleware
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', function(req, res) {
 	res.send('hey');
+});
+
+app.get('/login', function(req, res) {
+	// TODO: check if authorized first - no need for logged in user to go through TSA
+
+	res.render('login', { errors: [] });
+});
+app.post('/login', function(req, res) {
+	let { email, password } = req.body;
+	res.send(`Heres your JWT: ${email + password}`);
 });
 
 
