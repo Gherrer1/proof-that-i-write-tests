60b56475059fb3f3ab190dd9e239f16b08e66ffd
Gerardo Herrera <gherrer1@binghamton.edu>
2017-10-21 18:19:27 -0400

    fresh start

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,9 @@
 /* Have as little dependencies as possible here to make it testable */
 const User = require('../models/User');
 
-function createUser(validData, requiredFields, uniquenessVerifiers, passwordHasher) {
+function createUser(validData, passwordHasher) {
   return new Promise(function(resolve, reject) {
-    const dataMissing = requiredFields.some(field => !validData[field]);
-    if(dataMissing)
-      return reject(new Error('Some fields missing, cannot create user'));
-
-    uniquenessVerifiers({ email: validData.email, username: validData.username })
-      .then(function hashPassword() {
-        return passwordHasher(validData.password)
-      })
-      .then()
-      .catch(err => reject(err));
+    
   });
 }
 
