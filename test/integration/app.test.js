06c7f0bf12db7c48d4df1ac5118d1103ce871334
Gerardo Herrera <gherrer1@binghamton.edu>
2017-10-25 08:48:58 -0400

    cleaned up integration tests skeleton

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -6,6 +6,18 @@ const app = require('../../app');
 const seed = require('../../seed');
 
 describe('Authentication Routes', function() {
+
+  describe('GET /signup', function() {
+    it('should return an HTML file with a form field that we can regex');
+    it('should redirect to /dashboard it request has a session cookie');
+    it('should redirect -> /dashboard -> login if request has an invalid session cookie');
+  });
+
+  describe('POST /signup', function() {
+    it('should return the /signup HTML page with errors that we can regex');
+    it('should return the /signup HTML page with an error if we try to sign up with a taken username/email');
+    it('should redirect to /login with 200 status code if everything goes smoothly');
+  });
   // beforeEach(function(done) {
   //   seed.connect(function() {
   //     seed.seed()
@@ -18,17 +30,6 @@ describe('Authentication Routes', function() {
   //   // done(new Error('youre a cunt'));
   // });
 
-  it('should yata yata yata', function() {
-    throw new Error('cunt ass');
-  });
-
-  it('should throw yet another erro', function() {
-    // throw new Error('estephany');
-  });
-
-  it('should throw a 3rd', () => {
-    // throw new Error('baaaah');
-  });
 
   // afterEach(function(done) {
   //   seed.disconnect(done);
