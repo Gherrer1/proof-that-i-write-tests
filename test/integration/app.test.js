e4d7ff14775854e740631aa8d4abc72cf9563adc
Gerardo Herrera <gherrer1@binghamton.edu>
2017-10-24 21:52:35 -0400

    made skeleton for integration tests but first need to tweak the seed file

---
+++ test/integration/app.test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const should = chai.should();
+const sinon = require('sinon');
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Authentication Routes', function() {
+
+});
