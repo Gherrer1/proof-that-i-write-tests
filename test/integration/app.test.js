1f029b7640a7a4838f21351655eff2ed0b875643
Gerardo Herrera <gherrer1@binghamton.edu>
2017-10-31 23:09:53 -0400

    added debug to integration/app.test.js for more discerning logging

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -7,31 +7,32 @@ const app = require('../../src/app');
 const seed = require('../../seed');
 // const config = require('../../config');
 const {SESSION_COOKIE_NAME} = require('../../src/config');
+const debug = require('debug')('test-order');
 
 describe('#Authentication Routes', function() {
 
   beforeEach(function(done) {
-    console.log(':)');
+    debug(':)');
     seed.seed()
     .then(done, done);
   });
 
   afterEach(function(done) {
-    console.log(':(');
+    debug(':(');
     done();
   });
 
   describe('#GET /signup', function() {
     it('should redirect to /dashboard if request has a session cookie', function(done){
       // dont worry, itll run into authentication middleware to validate the cookie
-      console.log('running test');
+      debug('running test');
       request(app).get('/signup')
         .set('Cookie', [`${SESSION_COOKIE_NAME}=1234`])
         .expect(302)
         .expect('Location', '/dashboard', done);
     });
     it('should render an HTML file with an error message above the signup form if server-error cookie is present', function(done) {
-      console.log('running test');
+      debug('running test');
       const errorMessage = 'Something went wrong'
       request(app).get('/signup')
         .set('Cookie', ['server-error=Something went wrong'])
@@ -39,7 +40,7 @@ describe('#Authentication Routes', function() {
         .expect(/Something went wrong/, done);
     });
     it('should return an HTML file with a form field that we can regex (if no session cookie is present)', function(done){
-      console.log('running test');
+      debug('running test');
       request(app).get('/signup')
         .expect(200)
         .expect(/<ul class="signup-errors">/, done);
@@ -48,7 +49,7 @@ describe('#Authentication Routes', function() {
 
   describe('#GET /login', function() {
     it('should redirect to /dashboard if request has a session cookie', function(done) {
-      console.log('running test');
+      debug('running test');
       request(app).get('/signup')
         .set('Cookie', [`${SESSION_COOKIE_NAME}=1234`])
         .expect(302)
@@ -56,14 +57,14 @@ describe('#Authentication Routes', function() {
     });
     it('should show a client error message if request has a client-error-flash-message cookie');
     it('should show a success message if request has a success-flash-message cookie', function(done) {
-      console.log('running test');
+      debug('running test');
       request(app).get('/login')
         .set('Cookie', [`success=true`])
         .expect(200)
         .expect(/Successfully signed up!/, done);
     });
     it('should render an HTML file with a form field that we can regex (if no session cookie is present)', function(done) {
-        console.log('running test');
+        debug('running test');
         request(app).get('/login')
           .expect(200)
           .expect(/<form action="\/login" method="post">/, done);
@@ -72,14 +73,14 @@ describe('#Authentication Routes', function() {
 
   describe('#POST /signup', function() {
     it('should redirect to /dashboard if request has a session cookie', function(done) {
-      console.log('running test');
+      debug('running test');
       request(app).post('/signup')
         .set('Cookie', [`${SESSION_COOKIE_NAME}=1234`])
         .expect(302)
         .expect('Location', '/dashboard', done);
     });
     it('should redirect to /signup if request body has invalid parameters without any error messages to discourage bots - browser will have clientside validation', function(done) {
-      console.log('running test');
+      debug('running test');
       request(app).post('/signup')
         .send({ fname: 'Tester', email: 'invalid email lol', also: 'many fields are missing LOL' })
         .expect(302)
@@ -90,13 +91,13 @@ describe('#Authentication Routes', function() {
           }
           // heres where we check that no cookies were sent
           var cookies = res.headers['set-cookie'];
-          console.log(cookies);
+          debug(cookies);
           // TODO: actually check that no cookies were sent, we dont know the shape of the data right now
           done();
         });
     });
     it('should redirect to /signup if username or email in request body are not unique without error messages to discourage bots - clientside will check for uniqueness before allowing client to submit form', function(done) {
-      console.log('running test');
+      debug('running test');
       request(app).post('/signup')
         .send({ fname: 'Otherkirishima', email: 'kirishima@email.com', password: '1111111111', username: 'kirishima', passwordConfirmation: '1111111111' })
         .expect(302)
@@ -106,13 +107,13 @@ describe('#Authentication Routes', function() {
             return done(err);
           }
           var cookies = res.headers['set-cookie'];
-          console.log(cookies);
+          debug(cookies);
           // TODO: actually check that no cookies were returned (cookies that mightve carried error messages)
           done()
         });
     });
     it('should redirect to /login with success message if no session cookie and if request body is all valid, including unique username and email', function(done) {
-      console.log('running test');
+      debug('running test');
       request(app).post('/signup')
         .send({ fname: 'Uniqueuser', email: 'uniqueEmail@email.com', password: '1111111111', username: 'uniqueUsername', passwordConfirmation: '1111111111' })
         .expect(302)
@@ -130,14 +131,14 @@ describe('#Authentication Routes', function() {
 
   describe('#POST /login', function() {
     it('should redirect to /dashboard if request has a session cookie', function(done) {
-      console.log('running test');
+      debug('running test');
       request(app).post('/login')
         .set('Cookie', [`${SESSION_COOKIE_NAME}=1234`])
         .expect(302)
         .expect('Location', '/dashboard', done);
     });
     it('should redirect to /login with invalid-params cookie and tried-username cookie if data is invalid - as in simply doesnt fit the requirements used for signup', function(done) {
-      console.log('running test');
+      debug('running test');
       request(app).post('/login')
         .send({ email: 'a', password: '1' })
         .expect(302)
